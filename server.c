b6bf4c3d0700409193525fd62f4844cbb16d14d0
marineharutyunyan <m.marine.harutyunyan@gmail.com>
2022-10-10 14:25:13 +0400

    reconverting and printing message from binary to decimal

--- server.c
+++ server.c
@@ -4,34 +4,39 @@
 #include <unistd.h>
 
 int counter = 0;
-int *byte[7];
+int num = 0;
 
-void handler0() {
-	if (counter == 8)
-	{
-		write(1,"\n",1);
-		counter = -1;
-	}
-	byte[counter] = 0;
+void	handler0()
+{
 	counter++;
-	write(1,"0",1);
+	//write(1,"0",1);
+	if (counter > 7)
+	{
+		write(1, &num, 1);
+		num = 0;
+		counter = 0;
+	}
 }
 
-void handler1() 
+void	handler1()
 {
-	if (counter == 8)
-	{
-		write(1,"\n",1);
-		counter = -1;
-	}
-	byte[counter] = 1;
+	int power_of_two[] = {128, 64, 32, 16, 8, 4, 2, 1};
+
+	num += power_of_two[counter];
 	counter++;
-	write(1,"1",1);
+	//printf("\nnum is %d\n", num);
+	//write(1, "1", 1);
+	if (counter > 7)
+	{
+		write(1, &num, 1);
+		num = 0;
+		counter = 0;
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	int pid;
+	int	pid;
 
 	pid = getpid();
 	printf("Process Id is: %d\n", pid);
